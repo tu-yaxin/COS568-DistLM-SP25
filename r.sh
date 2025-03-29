2e600558fbf2c9cfe190191354ca99662355c4de
yaxintu <yaxintu@node-0.yt9801-dislm.cos568proj2-pg0.utah.cloudlab.us>
2025-03-28 20:39:50 -0600

    task2b_finish

--- r.sh
+++ r.sh
@@ -2,10 +2,10 @@ cd /proj/cos568proj2-PG0/groups/yt9801/COS568-DistLM-SP25/
 export GLUE_DIR=/proj/cos568proj2-PG0/glue_data
 export TASK_NAME=RTE
 # rm -r /tmp/2a/
-mkdir -p /tmp/2a/
-mkdir -p /tmp/2a/cache/ # you have to initialize a new cache folder each time or you are using outdated cache and your model crashes!!!!!!
+mkdir -p /tmp/2b/
+mkdir -p /tmp/2b/cache/ # you have to initialize a new cache folder each time or you are using outdated cache and your model crashes!!!!!!
 
-python3 run_glue2a.py \
+python3 run_glue2b.py \
   --model_type bert \
   --model_name_or_path bert-base-cased \
   --task_name $TASK_NAME \
@@ -16,8 +16,8 @@ python3 run_glue2a.py \
   --per_device_train_batch_size 16 \
   --learning_rate 2e-5 \
   --num_train_epochs 1 \
-  --output_dir /tmp/2a/ \
-  --cache_dir /tmp/2a/cache/ \
+  --output_dir /tmp/2b/ \
+  --cache_dir /tmp/2b/cache/ \
   --local_rank ${1} \
   --overwrite_output_dir \
   --master_ip 10.10.1.2 \
